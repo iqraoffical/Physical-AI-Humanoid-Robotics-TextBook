e2f4d82dd8b096cac63e47c760780d1972347f9b
iqra <iqraehsan926@gmail.com>
2025-12-07 15:41:55 +0500

    updated docusaurus config for github pages

--- docusaurus/my-textbook/docusaurus.config.ts
+++ docusaurus/my-textbook/docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://iqraoffical.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Physical-AI-Humanoid-Robotics-TextBook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'iqraoffical', // Usually your GitHub org/user name.
+  projectName: 'Physical-AI-Humanoid-Robotics-TextBook', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
